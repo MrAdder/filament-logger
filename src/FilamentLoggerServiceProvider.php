07e65be21f0dd96f70af4e4b60573efaa030e4a8
sescongene.dev@gmail.com <sescongene.dev@gmail.com>
2026-03-27 16:56:30 +0800

    chore: update GitHub repository link in install command

--- src/FilamentLoggerServiceProvider.php
+++ src/FilamentLoggerServiceProvider.php
@@ -53,7 +53,7 @@ class FilamentLoggerServiceProvider extends PackageServiceProvider
             ->hasInstallCommand(function (InstallCommand $installCommand) {
                 $installCommand
                     ->publishConfigFile()
-                    ->askToStarRepoOnGitHub('z3d0x/filament-logger')
+                    ->askToStarRepoOnGitHub('MrAdder/filament-logger')
                     ->startWith(function (InstallCommand $installCommand) {
                         $installCommand->call('vendor:publish', [
                             '--provider' => "Spatie\Activitylog\ActivitylogServiceProvider",
